47b119a27d16624bd17175b40e16582194006c78
GoldenGuy <36798446+IlyaLogunov@users.noreply.github.com>
2019-02-11 21:01:28 +0500

    Update welcome_get.php

--- welcome_get.php
+++ welcome_get.php
@@ -1,11 +1,8 @@
-<!DOCTYPE HTML>
 <html>
 <body>
-
 Welcome <?php echo $_GET["name"]; ?><br>
 Your email address is: <?php echo $_GET["email"]; ?>
 Your password is: <?php echo $_GET["psw"]; ?>
-
 </body>
 </html>
 
