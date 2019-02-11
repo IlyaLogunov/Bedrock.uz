99b82343651c796f75bdd818ca2749a341f417f5
GoldenGuy <36798446+IlyaLogunov@users.noreply.github.com>
2019-02-11 20:50:09 +0500

    Create welcome_get.php

---
+++ welcome_get.php
@@ -0,0 +1,11 @@
+<!DOCTYPE PHP>
+<html>
+<body>
+
+Welcome <?php echo $_GET["name"]; ?><br>
+Your email address is: <?php echo $_GET["email"]; ?>
+Your password is: <?php echo $_GET["psw"]; ?>
+
+</body>
+</html>
+
